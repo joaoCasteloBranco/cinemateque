0de79c76dd601a99a0a46a28f4967d55e87de6ed
João Victor Castelo Branco Gomes de Medeiros <joao.castelo.medeiros@gmail.com>
2024-12-02 14:55:22 -0300

    primeira visita

---
+++ spec/system/user_add_actor__spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+describe 'Usuário cadastra um ator ou atriz' do
+  it 'e com sucesso' do
+    # Arrange
+    user = FactoryBot.create(:user, email: 'joao@email.com')
+    login_as user
+
+    visit root_path
+    within('nav') do
+      click_on 'Artistas'
+    end
+    click_on "Adicionar Ator ou Atriz"
+    fill_in "Nome", with: "Fernanda Torres"
+    fill_in "Nacionalidade", with: "Brasileira"
+    fill_in "Data de Nascimento", with: "30/09/1960"
+    click_on "Adicionar Artista"
+
+    # Assert
+    expect(page).to have_content "Artista Adiciona com Sucesso"
+    expect(page).to have_content "Brasileira"
+    expect(page).to have_content "30/09/1960"
+  end
+end
